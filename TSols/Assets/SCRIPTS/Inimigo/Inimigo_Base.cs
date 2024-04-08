7aa023e849615fa00d75f34c2132c5754ed65875
RLH107 <110835307+RLH107@users.noreply.github.com>
2024-04-08 11:26:04 -0300

    Inimi

--- TSols/Assets/SCRIPTS/Inimigo/Inimigo_Base.cs
+++ TSols/Assets/SCRIPTS/Inimigo/Inimigo_Base.cs
@@ -6,6 +6,7 @@ public class Inimigo_Base : MonoBehaviour
 {
     [SerializeField] private string EnemyTipe;
     private float Health;
+    private float MaxHealth; 
     private float HealthCheck;
     private float Attack;
     private float Speed;
@@ -13,18 +14,54 @@ public class Inimigo_Base : MonoBehaviour
     void Start()
     {
         //https://forum.unity.com/threads/toggling-on-off-update-event.270879/
+
+        Health = 50f;
+        MaxHealth = 55f;
+        Attack = 5f;
+        Speed = 10f;
+        ActivationState = false;
+
+        Debug.Log(Health);
+
+        AddHealth(10f);
+
+        Debug.Log(Health);
+
+        TakeDamege(15f);
+
+        Debug.Log(Health);
     }
 
     private void TakeDamege(float DemegeTaken)
     {
-
+        Debug.Log("TakeDamge_CALLED");
+        Health -= DemegeTaken;
+        if (Health <= 0)
+        {
+            Dead();
+        }
     }
 
-    private void EnemyAttack(float AttackDemege)
+
+    private void AddHealth(float HealthToAdd)
     {
-
+        Debug.Log("addHealth_CALLED");
+        HealthCheck = Health;
+        HealthCheck += HealthToAdd;
+        if(HealthCheck < MaxHealth)
+        {
+            Debug.Log("If_CALLED");
+            Health += HealthToAdd;
+        }
+        else
+        {
+            Debug.Log("Else_CALLED");
+            Health = MaxHealth;
+        }
     }
 
-        //ADD Helth?
-        //
+    private void Dead()
+    {
+        Debug.Log("DEAD");
+    }
 }
