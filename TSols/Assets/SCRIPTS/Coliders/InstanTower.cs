63dc484e85eee2a15aee67fb3ab2d2edbcdf8c97
RLH107 <lucas.hellner08@gmail.com>
2024-05-20 17:11:07 -0300

    Mudanças

--- TSols/Assets/SCRIPTS/Coliders/InstanTower.cs
+++ TSols/Assets/SCRIPTS/Coliders/InstanTower.cs
@@ -15,6 +15,7 @@ public class InstanTower : MonoBehaviour
     private TodasTorres Tower_Script;
     private Vector3 StartingPos;
     private bool TowerState;
+    private MeshRenderer ThisMesh;
 
     //Timer For Button Debounce
     private float timer;
@@ -31,35 +32,37 @@ public class InstanTower : MonoBehaviour
     
     public void Touched()
     {
-        if (timer <= 0)
-        {
-            Debug.Log("Timer = "+timer);
-            Vibration.VibratePop();
-            // If Pressed ////////////////////////////
-            
-            //If Tower Is Inactive -> Activate Tower
-            if (TowerState == false)
-            {
-                Tower_Script.MoveTower(InsPOS.position);
-                Debug.Log("Activating Tower/ TowerPos = "+ ThisTower.transform.position + "InsTransform.position = "+ InsPOS);
-                TowerState = true;
-                StartCoroutine(TimerToZero());
-            }
-            //If Tower Is Active -> Deactivate Tower
-            if (TowerState == true)
-            {
-                Tower_Script.MoveTower(StartingPos);
-                Debug.Log("Deactivating Tower/ TowerPos = " + ThisTower.transform.position + "Initial Tower Transform.position = " + StartingPos);
-                TowerState = false;
-            }
-            //////////////////////////////////////////
-        }
+        bool Used = false;
         if (timer > 0)
         {
             Debug.Log("StillPressed");
+            timer = 0.05f;
         }
 
-        timer = 0.05f;
+        if (timer <= 0)
+        {
+            Vibration.VibratePop();
+            // If Pressed ////////////////////////////
+
+            //If Tower Is Active -> Deactivate Tower
+            if (TowerState == true && Used == false)
+            {
+                Tower_Script.MoveTower(StartingPos);
+                TowerState = false;
+                Used = true;
+            }
+            //If Tower Is Inactive -> Activate Tower
+            if (TowerState == false && Used == false)
+            {
+                Tower_Script.MoveTower(InsPOS.position);
+                TowerState = true;
+                Used = true;
+            }
+            timer = 0.05f;
+            StartCoroutine(TimerToZero());
+
+            //////////////////////////////////////////
+        }
     }
     IEnumerator TimerToZero()
     {
