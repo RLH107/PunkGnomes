e88284cefd6f2cc53f7f7ed8c94dbd1e028e8b79
RLH107 <110835307+RLH107@users.noreply.github.com>
2024-05-27 10:42:33 -0300

    Almost

--- TSols/Assets/SCRIPTS/LevelMeneger/WaweMaker.cs
+++ TSols/Assets/SCRIPTS/LevelMeneger/WaweMaker.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class WaweMaker : MonoBehaviour
 {
     [SerializeField] WawesControl Wawes_Script;
-
+    [SerializeField] private GameObject prefab_1;
+    [SerializeField] private GameObject prefab_2;
+    [SerializeField] private GameObject prefab_3;
+    [SerializeField] private GameObject prefab_4;
 
     void Start()
     {
-        
+        Wawes_Script.NumberOfWawes(1);
+        Wawes_Script.AddGameObjToListOfLists(0, prefab_1);
+
+        Wawes_Script.InsWawes();
     }
 
 }
--- TSols/Assets/SCRIPTS/LevelMeneger/WawesControl.cs
+++ TSols/Assets/SCRIPTS/LevelMeneger/WawesControl.cs
@@ -1,30 +1,116 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WawesControl : MonoBehaviour
 {
+    //Lists Of Lists
     private List<List<GameObject>> LOfObjLs; // List Of GameOjectcts Lists
     private List<List<int>> LOfIntLs; // List Of IntegerNumbers Lists
-    private GameObject Test;
 
 
+    //Wawes Lists
+    private List<List<GameObject>> InstantiatedObjects_OrWawes_ListOfLists;
 
-    public void StartWawesSistemTest()
+    //Lists within List of Lists
+    private List<GameObject> L_Obj;
+    private List<int> L_Int;
+
+    public void InsWawes()
     {
-
+        StartWawesSistemTest();
+        Instantiate_AllObjects_InListOfLists();  
     }
 
-    public void AddToGOListOfLists(int WhatListToAddTo, GameObject GO)
+    private void StartWawesSistemTest()
     {
-        List<GameObject> LGO = LOfObjLs[WhatListToAddTo];
-        LGO.Add(GO);
+        //Lists Can Not be 0
+        if (LOfObjLs.Count == 0)
+        {
+            Debug.LogError("GameObj ListOfLists can not be null");
+        }
+        if (LOfIntLs.Count == 0)
+        {
+            Debug.LogError("Int ListOfLists can not be null");
+        }
+
+        //Sees if Information In lists maches
+        if (LOfObjLs.Count != LOfIntLs.Count)
+        {
+            Debug.LogError("The Lists Of Lists Info does not match _Information Is Missing");
+        }
+
+        //For every List in ListOfLists
+        for (int i = 0; i < LOfObjLs.Count; i++)
+        {
+            //gets Lists From ListsOfLists
+            L_Obj = LOfObjLs[i];
+            L_Int = LOfIntLs[i];            
+            //Sees if Information In lists maches
+            if (L_Obj.Count != L_Int.Count)
+            {
+                Debug.LogError("Lists In ListsOfLists Positions " + i + "Info does not match __Information Is Missing");
+            }
+        }
+    }
+    private void Instantiate_AllObjects_InListOfLists()
+    {
+        //Temporary GameObject
+        GameObject thisObject;
+
+        //Creates a Temporery List
+        List<GameObject> L_Temp;
+        L_Temp = new List<GameObject>();
+
+        //For every member in {List Of GameOjectct Lists}
+        for (int i = 0;i < LOfObjLs.Count;i++)
+        {
+            //gets Lists From Lists Of Lists
+            L_Obj = LOfObjLs[i];
+            L_Int = LOfIntLs[i];
+
+            //For Every member in L_Obj instantiate Objects Requiered
+            for (int j = 0; j < L_Obj.Count; j++)
+            {
+                //For every Quantety in L_Int Instantiate L_Obj[j]
+                for (int k = 0; k < L_Int[j]; k++)
+                {
+                    //thisObject is the Instantiated(L_Obj[j]) Object
+                    thisObject = Instantiate(L_Obj[j], new Vector3(0, -100, 0), transform.rotation);
+                    // Adds thisObject to Temporary List
+                    L_Temp.Add(thisObject);
+                }
+            }
+            //Adds Temporary List To {InstantiatedObjects_OrWawes_ListOfLists}
+            InstantiatedObjects_OrWawes_ListOfLists.Add(L_Temp);
+            //Clears All Information From Temporary List
+            L_Temp.Clear();
+        }
+    }
+
+    public void NumberOfWawes(int Number)
+    {
+        for(int i = 0; i < Number; i++)
+        {
+            LOfObjLs[i] = new List<GameObject>();
+            LOfIntLs[i] = new List<int>();
+        }
+    }
+    
+
+    public void AddGameObjToListOfLists(int WhatListToAddTo, GameObject GO)
+    {
+        LOfObjLs[WhatListToAddTo].Add(GO);
     }
     public void AddToIntListOfLists(int WhatListToAddTo, int Int)
     {
         List<int> LIO = LOfIntLs[WhatListToAddTo];
         LIO.Add(Int);
     }
+    
+    //Return members?
+    /*
 
     private GameObject GetGameObjectInListOfLists(int ListOfListPos, int PosInList)
     {
@@ -36,4 +122,6 @@ public class WawesControl : MonoBehaviour
         List<int> LIO = LOfIntLs[ListOfListPos];
         return LIO[PosInList];
     }
+
+    */
 }
