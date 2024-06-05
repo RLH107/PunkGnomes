02cfed8eb1806f45eb3fa5a35e39a7145f27f178
RLH107 <lucas.hellner08@gmail.com>
2024-06-04 21:10:11 -0300

    fu

---
+++ TSols/Assets/SCRIPTS/LevelMeneger/LevelMenues.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    /*
+    
+    //// Begining Starts 
+    // Start menu = true
+    */
+
+    [SerializeField] private GameObject[] StartMenuArray; //Make all objects SetFalse
+    [SerializeField] private GameObject[] PlayMenuArray;
+    [SerializeField] private GameObject[] EndMenuArray;
+
+    /*
+    //// StartPlay on start wave
+    // deleged Start Wawes or Level 
+    
+    -Sets StartPlayMenu false
+    -Sets PlayMenu true
+    -Calls First Wave
+
+    //// 
+    
+    */
+
+    private void Start()
+    {
+        
+    }
+
+
+}
